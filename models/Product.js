aca39b7452e1b37cc6de1898fb7f35dcab585616
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-19 20:20:41 +0500

    Added Product model

---
+++ models/Product.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose(
+  {
+    name: {
+      type: String,
+      trim: true, // Remove the white space if present
+      required: [true, 'Please provide product name'],
+      maxlength: [100, 'Name can not be more than 100 characters'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Please provide product price'],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide product description'],
+      maxlength: [1000, 'Description can not be more than 1000 characters'],
+    },
+    image: {
+      type: String,
+      default: '/uploads/example.jpeg',
+    },
+    category: {
+      type: String,
+      required: [true, 'Please provide product category'],
+      enum: ['office', 'kitchen', 'bedroom'],
+    },
+    company: {
+      type: String,
+      required: [true, 'Please provide company'],
+      enum: {
+        values: ['ikea', 'liddy', 'marcos'],
+        message: '{VALUE} is not supported', // value which is not present in enum values
+      },
+    },
+    colors: {
+      type: [String],
+      default: ['#222'],
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+      default: 15,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Product', ProductSchema);
