6f1229576544599160994ed84ca7176cff35eda1
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-20 20:06:39 +0500

    Added remaining product controllers

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,25 +6,65 @@ const CustomError = require('../errors');
 const createProduct = async (req, res) => {
   // Assigning userID to a request body i.e req.body.user user is from product model
   req.body.user = req.user.userId;
+
+  // Create Product
   const product = await Product.create(req.body);
 
   res.status(StatusCodes.CREATED).json({ product });
 };
 
 const getAllProducts = async (req, res) => {
-  res.send('get all products');
+  // Get all the products
+  const products = await Product.find({});
+
+  res.status(StatusCodes.OK).json({ products, count: products.length });
 };
 
 const getSingleProduct = async (req, res) => {
-  res.send('get Single product');
+  const { id: productId } = req.params;
+
+  // Get the product with specific ID
+  const product = await Product.findOne({ _id: productId });
+
+  // Throw error if product is not present
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ product });
 };
 
 const updateProduct = async (req, res) => {
-  res.send('update product');
+  const { id: productId } = req.params;
+
+  // Update the product
+  const product = await Product.findOneAndUpdate({ _id: productId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  // Throw error if product is not present
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ product });
 };
 
 const deleteProduct = async (req, res) => {
-  res.send('delete product');
+  const { id: productId } = req.params;
+
+  // Get the product with specific ID
+  const product = await Product.findOne({ _id: productId });
+
+  // Throw error if product is not present
+  if (!product) {
+    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+  }
+
+  await product.remove();
+
+  res.status(StatusCodes.OK).json({ msg: 'Success! Product Removed.' });
 };
 
 const uploadImage = async (req, res) => {
