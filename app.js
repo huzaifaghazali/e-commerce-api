cf8174f6deab675765de17a6f41d2de31108dcb8
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-20 18:37:54 +0500

    Remove bug in route

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get('/api/v1', (req, res) => {
 // Routers middleware
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
-app.use('./api/v1/products', productRouter)
+app.use('/api/v1/products', productRouter)
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
