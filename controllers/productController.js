1421c04a00b6696d3d105179ff12cbac3161474b
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-20 19:14:54 +0500

    Added create product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,34 +1,41 @@
+const { StatusCodes } = require('http-status-codes');
 
+const Product = require('../models/Product');
+const CustomError = require('../errors');
 
-  const createProduct = async (req, res) => {
-   res.send('create product');
- };
- 
- const getAllProducts = async (req, res) => {
-   res.send('get all products');
- };
- 
- const getSingleProduct = async (req, res) => {
-   res.send('get Single product');
- };
- 
- const updateProduct = async (req, res) => {
-   res.send('update product');
- };
- 
- const deleteProduct = async (req, res) => {
-   res.send('delete product');
- };
- 
- const uploadImage = async (req, res) => {
-   res.send('upload image');
- };
- 
- module.exports = {
-    createProduct,
-    getAllProducts,
-    getSingleProduct,
-    updateProduct,
-    deleteProduct,
-    uploadImage
- }
+const createProduct = async (req, res) => {
+  // Assigning userID to a request body i.e req.body.user user is from product model
+  req.body.user = req.user.userId;
+  const product = await Product.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ product });
+};
+
+const getAllProducts = async (req, res) => {
+  res.send('get all products');
+};
+
+const getSingleProduct = async (req, res) => {
+  res.send('get Single product');
+};
+
+const updateProduct = async (req, res) => {
+  res.send('update product');
+};
+
+const deleteProduct = async (req, res) => {
+  res.send('delete product');
+};
+
+const uploadImage = async (req, res) => {
+  res.send('upload image');
+};
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+  uploadImage,
+};
