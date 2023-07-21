231029b999d821e2819246978a75e77aae6c64b5
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-22 00:35:17 +0500

    Added get all and single reviews controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -42,11 +42,24 @@ const createReview = async (req, res) => {
 };
 
 const getAllReviews = async (req, res) => {
-  res.send('Get All Review');
+  // Get all the reviews
+  const reviews = await Review.find({});
+
+  res.status(StatusCodes.OK).json({ reviews, count: reviews.length });
 };
 
 const getSingleReview = async (req, res) => {
-  res.send('Get Single Review');
+  const { id: reviewId } = req.params;
+
+  // Get the specific review
+  const review = await Review.findOne({ _id: reviewId });
+
+  // Throw error if review is not present
+  if (!review) {
+    throw new CustomError.NotFoundError(`No review with id ${reviewId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ review });
 };
 
 const updateReview = async (req, res) => {
