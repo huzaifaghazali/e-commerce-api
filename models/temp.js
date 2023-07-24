3550fc36d15ab3e182280c9d38b13959b618d497
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-24 19:26:05 +0500

    Added temp file for aggregate pipline

---
+++ models/temp.js
@@ -0,0 +1,36 @@
+import { MongoClient } from 'mongodb';
+import {
+  ObjectId
+} from 'mongodb';
+
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+
+const agg = [
+  {
+    '$match': {
+      'product': new ObjectId('64be8134b23b1a932959c2e5')
+    }
+  }, {
+    '$group': {
+      '_id': null, 
+      'averageRating': {
+        '$avg': '$rating'
+      }, 
+      'numOfReviews': {
+        '$sum': 1
+      }
+    }
+  }
+];
+
+const client = await MongoClient.connect(
+  '',
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const coll = client.db('E-COMMERCE-1').collection('reviews');
+const cursor = coll.aggregate(agg);
+const result = await cursor.toArray();
+await client.close();
