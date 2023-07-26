b0dcf9f25daeb6a0706b1ad157fcfa43e09feb38
huzaifaghazali <huzaifaghazali@gmail.com>
2023-07-26 21:03:41 +0500

    Added security packages

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ const app = express();
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
+  // Security packages
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
 
 // database
 const connectDB = require('./db/connect');
@@ -25,6 +31,18 @@ const orderRouter = require('./routes/orderRoutes');
 // middleware
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
 
 app.use(morgan('tiny')); // logging request details
 app.use(express.json()); // It parses incoming JSON requests and puts the parsed data in req.body
